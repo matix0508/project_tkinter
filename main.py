70505cc19b0bc1eadecb1e4afce0e74b64f05083
matix0508 <matix0508@gmail.com>
2020-05-03 16:41:07 +0200

    MyApp(Tk)

--- main.py
+++ main.py
@@ -8,4 +8,21 @@ class MyApp(Tk):
 
         Tk.wm_title(self, 'My title')
 
-        
+        container = Frame(self)
+        container.pack(side='top', fill='both', expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (HomePage, PageOne):  # pętla do wrzucania klatek do self.frames
+            frame = F(container, self)
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky='nsew')
+
+        self.show_frame(HomePage)
+
+    def show_frame(self, cont):
+        frame = self.frame[cont]
+        frame.tkraise()
