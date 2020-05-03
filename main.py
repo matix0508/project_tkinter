f3368e7b19fac7cdfb3e032ae28a632f524a7d3b
matix0508 <matix0508@gmail.com>
2020-05-03 16:57:23 +0200

    working app

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from tkinter import Tk, Frame, Label, Button
 
+LARGE_FONT = ('Verdana', 12)
+
+WELCOME_TEXT = """long text
+dijwid
+ajsidjasi
+aidhaid
+asdkhahsk"""
+
 
 class MyApp(Tk):
 
-    def __init__(self, *args, **kwagrs):
+    def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
 
         Tk.wm_title(self, 'My title')
@@ -24,5 +32,35 @@ class MyApp(Tk):
         self.show_frame(HomePage)
 
     def show_frame(self, cont):
-        frame = self.frame[cont]
+        frame = self.frames[cont]
         frame.tkraise()
+
+class HomePage(Frame):
+
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+
+        label = Label(self, text='HomePage', font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = Button(self, text="Visit Page1",
+                        command=lambda: controller.show_frame(PageOne))
+        button1.pack()
+
+        welcome_text = Label(self, text=WELCOME_TEXT)
+        welcome_text.pack()
+
+class PageOne(Frame):
+
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+
+        label = Label(self, text='Page One', font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = Button(self, text="Home Page",
+                        command=lambda: controller.show_frame(HomePage))
+        button1.pack()
+
+app = MyApp()
+app.mainloop()
