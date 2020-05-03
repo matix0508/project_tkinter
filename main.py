55762ed605f9ebff180bf50f182d034cd35e70f4
matix0508 <matix0508@gmail.com>
2020-05-03 16:30:37 +0200

    MyApp init

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from tkinter import Tk, Frame, Label, Button
+
+
+class MyApp(Tk):
+
+    def __init__(self, *args, **kwagrs):
+        Tk.__init__(self, *args, **kwargs)
+
+        Tk.wm_title(self, 'My title')
+
+        
